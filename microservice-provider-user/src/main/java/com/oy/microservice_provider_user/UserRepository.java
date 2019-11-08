30a0dba5940c8e0255dfddcaaad1879f129b24f2
OyXw1 <992543768@qq.com>
2019-11-08 11:52:19 +0800

    delete microservice-provider-user dir

--- microservice-provider-user/src/main/java/com/oy/microservice_provider_user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.oy.microservice_provider_user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
